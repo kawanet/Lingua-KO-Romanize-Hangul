192df049fff86148d31afe4c15fa7a6ff8c277f0
kawa0117 <kawa0117@83962b45-0e43-0410-a248-1981e341774c>
2008-01-13 16:13:22 +0000

    [KO] 0.20 RC1
    git-svn-id: https://linuga-romanize.googlecode.com/svn/trunk/Lingua-KO-Romanize-Hangul@12 83962b45-0e43-0410-a248-1981e341774c

--- lib/Lingua/KO/Romanize/Hangul.pm
+++ lib/Lingua/KO/Romanize/Hangul.pm
@@ -23,6 +23,8 @@ Lingua::KO::Romanize::Hangul - Romanization of Korean language
 =head1 DESCRIPTION
 
 Hangul is phonemic characters of the Korean language.
+This module follows the C<Revised Romanization of Korean> which
+was released on July 7, 2000 as the official system in South Korea.
 
 =head2 $conv = Lingua::KO::Romanize::Hangul->new();
 
@@ -47,43 +49,47 @@ which are pairs of a Hangul chacater and its romanized letters.
     $array[1][0]        # secound Korean character itself
     $array[1][1]        # its romanized letters
 
+=head1 UTF-8 FLAG
+
+This module treats utf8 flag transparently.
+
 =head1 SEE ALSO
 
-L<Lingua::JA::Romanize::Japanese>
-L<Lingua::ZH::Romanize::Pinyin>
+L<Lingua::JA::Romanize::Japanese> for Japanese
+
+L<Lingua::ZH::Romanize::Pinyin> for Chinese
+
+http://www.korean.go.kr/06_new/rule/rule06.jsp
 
 http://www.kawa.net/works/perl/romanize/romanize-e.html
 
-=head1 AUTHOR
-
-Yusuke Kawasaki http://www.kawa.net/
-
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 1998-2006 Yusuke Kawasaki. All rights reserved.
-
-This program is free software; you can redistribute it
-and/or modify it under the same terms as Perl itself.
+Copyright (c) 1998-2008 Yusuke Kawasaki. All rights reserved.
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
 
 =cut
 # ----------------------------------------------------------------
-    package Lingua::KO::Romanize::Hangul;
-    use strict;
-    use vars qw( $VERSION );
-    $VERSION = "0.12";
-# ----------------------------------------------------------------
-    my $INITIAL_LETTER = [map {$_ eq "-" ? "" : $_} qw(
-        g   kk  n   d   tt  r   m   b   bb  s   ss  -   j   jj
-        c   k   t   p   h
-    )];
-    my $PEAK_LETTER = [map {$_ eq "-" ? "" : $_} qw(
-        a   ae  ya  yae eo  e   yeo ye  o   wa  wae oe  yo  u
-        weo we  wi  yu  eu  yi  i
-    )];
-    my $FINAL_LETTER = [map {$_ eq "-" ? "" : $_} qw(
-        -   g   kk  ks  n   nj  nh  d   r   rg  rm  rb  rs  rt
-        rp  rh  m   b   bs  s   ss  ng  j   c   k   t   p   h
-    )];
+package Lingua::KO::Romanize::Hangul;
+use strict;
+use vars qw( $VERSION );
+$VERSION = "0.20";
+my $PERL581 = 1 if ( $] >= 5.008001 );
+
+my $INITIAL_LETTER = [map {$_ eq '-' ? '' : $_} qw(
+    g   kk  n   d   tt  r   m   b   pp  s   ss  -   j   jj
+    ch  k   t   p   h
+)];
+my $PEAK_LETTER = [map {$_ eq '-' ? '' : $_} qw(
+    a   ae  ya  yae eo  e   yeo ye  o   wa  wae oe  yo  u
+    wo  we  wi  yu  eu  ui  i
+)];
+my $FINAL_LETTER = [map {$_ eq '-' ? '' : $_} qw(
+    -   g   kk  ks  n   nj  nh  d   r   rg  rm  rb  rs  rt
+    rp  rh  m   b   bs  s   ss  ng  j   c   k   t   p   h
+)];
+
 # ----------------------------------------------------------------
 sub new {
     my $package = shift;
@@ -91,8 +97,19 @@ sub new {
     bless $self, $package;
     $self;
 }
-# ----------------------------------------------------------------
+
 sub char {
+    my $self = shift;
+    return $self->_char(@_) unless $PERL581;
+    my $char = shift;
+    my $utf8 = utf8::is_utf8( $char );
+    utf8::encode( $char ) if $utf8;
+    $char = $self->_char( $char );
+    utf8::decode( $char ) if $utf8;
+    $char;
+}
+
+sub _char {
     my $self = shift;
     my $char = shift;
     my( $c1, $c2, $c3, $c4 ) = unpack("C*",$char);
@@ -106,18 +123,34 @@ sub char {
     my $fin  = $han % 28;
     join( "", $INITIAL_LETTER->[$init], $PEAK_LETTER->[$peak], $FINAL_LETTER->[$fin] );
 }
-# ----------------------------------------------------------------
+
 sub chars {
     my $self = shift;
     my @array = $self->string( shift );
     join( " ", map {$#$_>0 ? $_->[1] : $_->[0]} @array );
 }
-# ----------------------------------------------------------------
+
+sub string {
+    my $self = shift;
+    return $self->_string(@_) unless $PERL581;
+    my $char = shift;
+    my $flag = utf8::is_utf8( $char );
+    utf8::encode( $char ) if $flag;
+    my @array = $self->_string( $char );
+    if ( $flag ) {
+        foreach my $pair ( @array ) {
+            utf8::decode( $pair->[0] ) if defined $pair->[0];
+            utf8::decode( $pair->[1] ) if defined $pair->[1];
+        }
+    }
+    @array;
+}
+
 #   [UCS-2] AC00-D7A3
 #   [UTF-8] EAB080-ED9EA3
 #   EA-ED are appeared only as Hangul's first character.
-# ----------------------------------------------------------------
-sub string {
+
+sub _string {
     my $self = shift;
     my $src = shift;
     my $array = [];
@@ -131,8 +164,54 @@ sub string {
             push( @$array, [ $2 ] );
         }
     }
+
+    for ( my $i = 0 ; $i < $#$array ; $i++ ) {
+        next if ( scalar @{ $array->[$i] } < 2 );
+        next if ( scalar @{ $array->[ $i + 1 ] } < 2 );
+        my $this = $array->[$i]->[1];
+        my $next = $array->[ $i + 1 ]->[1];
+        my $novowel = 1 unless ( $next =~ /^[aeouiwy]/ );
+
+        if ( $this =~ /(tt|pp|jj)$/ && $novowel ) {
+            $array->[$i]->[1] =~ s/(tt|pp|jj)$//;
+        }
+        elsif ( $this =~ /([^n]g|kk)$/ && $novowel ) {
+            $array->[$i]->[1] =~ s/(g|kk)$/k/;
+        }
+        elsif ( $this =~ /(d|j|ch|s?s)$/ && $novowel ) {
+            $array->[$i]->[1] =~ s/(d|j|ch|s?s)$/t/;
+        }
+        elsif ( $this =~ /(b)$/ && $novowel ) {
+            $array->[$i]->[1] =~ s/(b)$/p/;
+        }
+        elsif ( $this =~ /(r)$/ && $novowel ) {
+            $array->[$i]->[1] =~ s/(r)$/l/;
+            $array->[$i+1]->[1] =~ s/^r/l/;
+        }
+    }
+
+    if ( scalar @$array ) {
+        my $last = $array->[$#$array];
+        my $this = $last->[1];
+        if ( $this =~ /(tt|pp|jj)$/ ) {
+            $last->[1] =~ s/(tt|pp|jj)$//;
+        }
+        elsif ( $this =~ /([^n]g|kk)$/ ) {
+            $last->[1] =~ s/(g|kk)$/k/;
+        }
+        elsif ( $this =~ /(d|j|ch|s?s)$/ ) {
+            $last->[1] =~ s/(d|j|ch|s?s)$/t/;
+        }
+        elsif ( $this =~ /(b)$/ ) {
+            $last->[1] =~ s/(b)$/p/;
+        }
+        elsif ( $this =~ /(r)$/ ) {
+            $last->[1] =~ s/(r)$/l/;
+        }
+    }
+
     @$array;
 }
+
 # ----------------------------------------------------------------
 ;1;
-# ----------------------------------------------------------------
--- t/01_hangul.t
+++ t/01_hangul.t
@@ -1,10 +1,16 @@
 # ----------------------------------------------------------------
     use strict;
-    use Test::More tests => 8;
-    BEGIN { use_ok('Lingua::KO::Romanize::Hangul'); };
+    use Test::More;
 # ----------------------------------------------------------------
 {
+    plan tests => 8;
+    use_ok('Lingua::KO::Romanize::Hangul');
     my $roman = Lingua::KO::Romanize::Hangul->new();
+    &test_ko( $roman );
+}
+# ----------------------------------------------------------------
+sub test_ko {
+    my $roman = shift;
     ok( ref $roman, "new" );
 
     ok( (! defined $roman->char("a")), "char: ascii" );
@@ -12,11 +18,11 @@
     is( $roman->char("\xED\x9E\xA3"), "hih", "char: hih (D7A3)" );
 
     my $c4 = $roman->chars("\xec\x97\xac\xeb\xb3\xb4\xec\x84\xb8\xec\x9a\x94");
-    is( $c4, "yeo bo se yo", "chars: yeoboseyo [$c4]" );
+    is( $c4, "yeo bo se yo", "chars: yeoboseyo" );
 
     my @s1 = $roman->string("\xED\x95\x9C\xEA\xB8\x80");
-    is( $s1[0][1], "han", "string: han [$s1[0][1]]" );
-    is( $s1[1][1], "geur", "string: geur [$s1[1][1]]" );
+    is( $s1[0][1], "han", "string: han" );
+    is( $s1[1][1], "geul", "string: geul" );
 }
 # ----------------------------------------------------------------
 ;1;
